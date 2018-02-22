eb01f501d807a1b14f7b8fa41a7adccc57168e7a
Rosanma4 <rosanma4@epsa.upv.es>
2018-02-22 10:29:32 +0100

    repaso examen
    repaso examen
    
    Co-Authored-By: rosanma <rosanma@users.noreply.github.com>

--- main_repaso.js
+++ main_repaso.js
@@ -1,10 +1,8 @@
 //funcion sincrona main()
 var invertir = require("./repaso_jordi.js");
-console.log(res);
-
 // funcion asincrona main()
-var res = invertir("hola")
+var res = invertir.invertir("hola")
 console.log(res);
-invertir2("hola otra vez", function (resultado){
+invertir.invertir2("hola otra vez", function (resultado){
   console.log("dentro del callback:" + resultado)
 });
