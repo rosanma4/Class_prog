bf23764ae76dd11a77119948ba92bfd78ef083a2
Rosanma4 <rosanma4@epsa.upv.es>
2018-02-22 10:05:25 +0100

    Programación
    repaso examen
    
    Co-Authored-By: rosanma <rosanma@users.noreply.github.com>

--- repaso_jordi.js
+++ repaso_jordi.js
@@ -1,21 +1,26 @@
 //Función Sincrona()
 function invertir (palabra){
 var res = []
-var tam = palabra.length()
-  for (var i = 0; i <= tam -1; i++) {
-    res = res + palabra[tam -1 -1]
+  for (var i = 0; i <= palabra.length -1; i++) {
+    res = res + palabra[palabra.length -1 -i]
   }
+  return res;
 }
 //main()
 var res = invertir("hola mundo");
 console.log(res);
-
-///Funcion Asincrona()
-function invertir2 (texto2, callback){
-
+//Funcion Asincrona()
+function invertir2 (palabra, callback){
+  var res=[]
+  for (var i = 0; i <= palabra.length -1; i++) {
+  res = res+  palabra[palabra.length-i-1]
+}
+callback(res)
 }
 //main()
 var res = invertir("hola")
 invertir2("hola otra vez", function (resultado){
-
+  console.log("dentro del callback:" + resultado)
 });
+//fin funciones
+console.log("final funciones sincronas y asincronas")
