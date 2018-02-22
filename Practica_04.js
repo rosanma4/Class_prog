23f47b2d89480f021372fb3419fff6e666ddf33f
Rosanma4 <rosanma4@epsa.upv.es>
2018-02-22 09:57:53 +0100

    practica1

---
+++ Practica_04.js
@@ -0,0 +1,16 @@
+// array asociativo
+var a = []
+console.log(a ["uno"] = 1)
+console.log(a ["dos"] = "segundo")
+var persona = []
+console.log(persona ["nombre"] = "Roman")
+console.log(persona["edad"] = 99)
+console.log(persona.nombre)
+persona["saluda"] = function (){
+  console.log ("hola");
+}
+persona.saluda();
+//--------------------------------------------
+//--------------------------------------------
+//--------------------------------------------
+//--------------------------------------------
