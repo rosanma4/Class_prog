5e96e5d6cd239fd7faf86411384172822d8662d8
Rosanma4 <rosanma4@epsa.upv.es>
2018-03-22 10:34:30 +0100

    trabajo pascua

---
+++ conjunto.js
@@ -0,0 +1,47 @@
+//Añadir (eliminar un elemento, unir, intersectar, diferencia, diferencia simetrica)
+//Hacer test por cada implementación
+
+
+const NO_ESTA = -1
+
+// explicación
+
+class Conjunto{
+
+  constructor(){
+    //creamos una lista vacia y la guardamos
+    //en la variable de losElementos
+    this.losElementos = []
+  }
+  //Diseño talla
+  //talla () --> N
+  talla(){
+    return this.losElementos.length;
+  }
+  //R-->DondeEsta()-->Entero// NO ESTA(posicion donde esta, si no esta devuelve un numero negativo)
+  DondeEsta(num){
+    for (var i = 0; i <= this.losElementos[i]length-1; i++) {
+      if(num== this.losElementos[i]){
+        //bingo
+        return i //tornem on esta, i acabe
+      }//if
+    }//for
+    return NO_ESTA
+
+  }
+  //R--> contiene --> V/F
+  //si no contiene numero
+  contiene(num){
+    if(this.DondeEsta(num) == NO_ESTA){
+      return false;
+
+    }
+    return true
+  }
+  //R --> añadir()
+  añadir(num){
+    if(! this.contiene(num)){
+      this.losElementos.push(num)
+    }
+  }
+}//class
