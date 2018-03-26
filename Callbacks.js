e840857bf698ebf097d6b0c61843214068d4c9af
Rosanma4 <rosanma4@epsa.upv.es>
2018-03-26 21:49:20 +0200

    callbacksejercici

---
+++ Callbacks.js
@@ -0,0 +1,12 @@
+//----------------------------------------------
+// Procesar un array
+
+var zfighters = ["Goku", "Vegeta", "Gohan", "Piccolo"];
+zfighters.forEach(function(name,index){
+  console.log(index + 1 + ". " + name)
+})
+
+// Fin Procesar un array
+//----------------------------------------------
+console.log("********************************************************");
+//----------------------------------------------
