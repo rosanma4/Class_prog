4c0ffd71ab4bfae96888539d46ac1e4fd9715ddb
Rosanma4 <rosanma4@epsa.upv.es>
2018-03-26 21:56:58 +0200

    ejercicios

--- Callbacks.js
+++ Callbacks.js
@@ -1,8 +1,31 @@
 //----------------------------------------------
+//Ejercicio Sencillo callback
+var inicio = function (valor, callback){
+  if(typeof callback === 'function'){ // Para verificar que callback es una función
+    callback(valor);
+  }else{
+    console.log(valor);
+  }
+}
+inicio ("Román", "asdasa");
+// otra manera
+console.log("********************************************************");
+var inicio = function (valor, callback){
+  if(typeof callback === 'function'){ // Para verificar que callback es una función y asi no da error 
+    callback(valor);
+  }else{
+    console.log(valor);
+  }
+}
+inicio ("Raquel", function(valor){
+  console.log("Bienvenido "+ valor)
+});
+//----------------------------------------------
+console.log("********************************************************");
 // Procesar un array
 
 var zfighters = ["Goku", "Vegeta", "Gohan", "Piccolo"];
-zfighters.forEach(function(name,index){
+  zfighters.forEach(function(name,index){
   console.log(index + 1 + ". " + name)
 })
 
