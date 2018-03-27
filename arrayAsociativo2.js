17bbb05fae866e85cd9ed588a74e3dbe0b64f40d
Rosanma4 <rosanma4@epsa.upv.es>
2018-03-27 10:58:31 +0200

    wqewq

--- arrayAsociativo2.js
+++ arrayAsociativo2.js
@@ -23,9 +23,9 @@ for (var i in datosPersonales){
   console.log(i+" ->" + datosPersonales[i])
 }*/
 //------------------------------
-for (var i in ejemplo){
-  for (var j in ejemplo[i]){
-
+for (var i in ejemplo){ // Muestra el primer objeto (persona, coche)
+  for (var j in ejemplo[i]){ // Muestra el objeto que esta dentro del primer objeto
+    //Si el tamaño del arrray es mayor de 1 muestra por pantalla el valor de las casillas
      if (ejemplo[i][j].length > 1) {
       for (const value of ejemplo[i][j]) {
         console.log(value);
