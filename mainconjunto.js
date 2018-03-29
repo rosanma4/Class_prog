9a17a309106012df3599f7afd573e7ff01b911f2
Rosanma4 <rosanma4@epsa.upv.es>
2018-03-29 20:23:22 +0200

    conjunto

--- mainconjunto.js
+++ mainconjunto.js
@@ -2,11 +2,20 @@ module.exports = Conjunto;
 var Conjunto = require("./conjunto.js");
 
 var c1 = new Conjunto();
+var c2 = new Conjunto();
+
+//Añadir conjunto
 c1.anadir(1);
+c1.anadir(2);
 c1.anadir(5);
-c1.anadir(6);
+c2.anadir(3);
+c2.anadir(4);
 
-
+//Declarar Funciones
 var t = c1.talla();
+var o = c2.talla();
 
-console.log(t);
+
+//Imprimir por Pantalla
+console.log("Conjunto 1 --> " + t);
+console.log("Conjunto 2 --> " + o);
