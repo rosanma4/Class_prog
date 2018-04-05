5536fd01c6c0b729c32b2c71daf591b8f3d0324c
Rosanma4 <rosanma4@epsa.upv.es>
2018-04-05 20:49:25 +0200

    dasdas

--- Raquel_Conjunto.js
+++ Raquel_Conjunto.js
@@ -1,3 +1,5 @@
+//Información Obtenido de la página https://developer.mozilla.org/es/docs/Web/JavaScript/Referencia/Objetos_globales/Array
+//utilizando las distintas opciones que se dan.
 const NO_ESTA = -1
 
 module.exports = class Conjunto{
