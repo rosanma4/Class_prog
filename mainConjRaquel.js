e9d2ea529840af163384d7e706d23f5111cbf009
Rosanma4 <rosanma4@epsa.upv.es>
2018-04-06 16:43:25 +0200

    cambios

--- mainConjRaquel.js
+++ mainConjRaquel.js
@@ -7,7 +7,6 @@ c1.anadir(9);
 c1.anadir(7);
 c1.anadir(6);
 c1.anadir(3);
-console.log(c1);
 var t = c1.talla();
 console.log("El tamaño del conjunto sera =  " + t);
 console.log(c1);
@@ -17,7 +16,7 @@ console.log(c1);
 console.log("El array contiene  el 6 =  " + c1.contiene(6));
 console.log("El array contiene el 3 =  " + c1.contiene(3));
 
-
+console.log("El conjunto 2 estara formado por 8, 210, 23");
 var unir = c1.unir([8,210,23]);
 console.log("Si unimos dos conjuntos nos dara = " + unir);
 
@@ -25,7 +24,7 @@ var diff = c1.diferencia([3, 10, 5]);
 console.log("Que diferencia habra entre los dos conjuntos = " + diff);
 
 //Vaciar un conjunto
-
+console.log("Si vaciamos el arrat nos saldra:");
 c1.vaciar();
 console.log(c1);
 //Funciones
