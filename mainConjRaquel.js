51c1ea8feb3f4682f2c3dcf022bf8bb5517ee93b
Rosanma4 <rosanma4@epsa.upv.es>
2018-04-06 17:26:28 +0200

    separacion

--- mainConjRaquel.js
+++ mainConjRaquel.js
@@ -7,26 +7,33 @@ c1.anadir(9);
 c1.anadir(7);
 c1.anadir(6);
 c1.anadir(3);
+console.log("--------------");
 var t = c1.talla();
 console.log("El tamaño del conjunto sera =  " + t);
 console.log(c1);
+console.log("--------------");
 console.log("Eliminar el 3 del array");
 c1.eliminar(3);
 console.log(c1);
+console.log("--------------");
 console.log("El array contiene  el 6 =  " + c1.contiene(6));
 console.log("El array contiene el 3 =  " + c1.contiene(3));
+console.log("--------------");
 
 console.log("El conjunto 2 estara formado por 8, 210, 23");
 var unir = c1.unir([8,210,23]);
 console.log("Si unimos dos conjuntos nos dara = " + unir);
+console.log("--------------");
 
 var diff = c1.diferencia([3, 10, 5]);
 console.log("Que diferencia habra entre los dos conjuntos = " + diff);
+console.log("--------------");
 
 //Vaciar un conjunto
 console.log("Si vaciamos el arrat nos saldra:");
 c1.vaciar();
 console.log(c1);
+console.log("--------------");
 //Funciones
 
 var t = c1.talla();
