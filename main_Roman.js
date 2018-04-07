acf122c75049e0682a9a671ca70d03aea90b3890
Rosanma4 <rosanma4@epsa.upv.es>
2018-04-07 16:19:46 +0200

    modificaciones

--- main_Roman.js
+++ main_Roman.js
@@ -9,8 +9,9 @@ var t = con1.talla();
 console.log("El conjunto sera de " + t +" valores");
 console.log(con1);
 con1.contiene(3);
-console.log("El conjunto contiene el valor 3? --> " + con1.contiene(3));
-console.log("El conjunto contiene el valor 6? --> " + con1.contiene());
+console.log("El conjunto contiene el valor 1? --> " + con1.contiene(1));
+console.log("El conjunto contiene el valor 50? --> " + con1.contiene(50));
+console.log("El conjunto contiene el valor 7? --> " + con1.contiene(7));
 con1.anadir(4);
 console.log("Damos otro valor al array en este caso un 4");
 console.log(con1);
@@ -20,7 +21,7 @@ console.log(con1);
 console.log("Vamos a realizar la union de dos conjuntos con los valores [5,6,7]")
 var unir = con1.unir([5,6,7]);
 console.log("Al realizar la unión se podra observar: " + unir);
-console.log("Ahora pondremos en practica el metodo vaciar un array")
+console.log("Ahora pondremos en practica el metodo vaciar un array");
 con1.vaciar();
 var t = con1.talla();
 console.log("El conjunto sera de " + t +" valores");
