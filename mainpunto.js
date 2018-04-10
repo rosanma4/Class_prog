8159b2d4c7de53cf51c02e25a8a0e56c550d556a
Rosanma4 <rosanma4@epsa.upv.es>
2018-04-10 12:28:03 +0200

    asas

--- mainpunto.js
+++ mainpunto.js
@@ -1,12 +1,9 @@
 var PUNTO = require ("./punto.js")
 var punto1 = new PUNTO (1,2);
 var punto2 = new PUNTO (30,2);
-console.log("**************");
+console.log(punto1);
+console.log(punto2);
 console.log(punto1.getX());
-console.log("**************");
 console.log(punto1.getY());
-console.log("**************");
 console.log(punto1.distancia(punto2));
-console.log("**************");
 console.log(punto1.suma(punto2));
-console.log("**************");
