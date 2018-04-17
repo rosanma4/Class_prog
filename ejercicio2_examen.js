816d410fb7d38c6ab90937a32f080e14014ad092
Rosanma4 <rosanma4@epsa.upv.es>
2018-04-17 12:34:32 +0200

    Examen 2 - preguntas

---
+++ ejercicio2_examen.js
@@ -0,0 +1,47 @@
+//----------------------------------Pregunta 1----------------------------------
+
+//Diseña una función que sirva para darle la vuelta a una lista de elementos,
+//conservando intacta la lista original.
+//var lista = [1,22,333]
+//Entonces, trás la ejecución de la función debe haber una lista IRes que
+//contenga [333,22,1]
+//Implementa la función de forma asíncrona sin utlizar ni return ni callback.
+//Escribe una llamada a la función.
+//Implementa la función utlizando un callback para devolver el resultado.
+//Escribe una llamada a la función.
+
+//----------------------------------Pregunta 2----------------------------------
+
+//Diseña el método diferencia() de la clase conjunto que obtenga un nuevo
+//conjunto C resultado de "restar" otro dos a-b. Ten en cuenta que uno de esos
+//dos conjuntos será el objeto sobre el que se llamará el método diferencia().
+//Escribe una llamada al método diferencia().
+//Cómo el método con contiene() (R -> Contiene() -> V/F)ya esta implementao y
+//uno de los grandes lemas de la programación es "don't repeat yourself" ("no
+//vuelvas a inventar la rueda"), vamos a utilizar el siguiente algoritmo para
+//la implementación.
+//Implementa el método diferencia() siguiendo el anterior algoritmo y
+//consecuentemente utilizando los métodos contiene() y anyadir().
+
+//----------------------------------Pregunta 3----------------------------------
+
+//Dada una palabra, está claro que podemos cambiar una funcion que obtenga
+//un entero a partir de ella. Por ejemplo podemos calcular su longitud:
+//o cuantas vocales tiene, etc ¿Cual es el diseño de una funcion asi?
+//Si tenemos una lista de palabras podemos escribir una función aplicar() que
+//obtenga una lista de enteros, aplicando a cada palabra de una lista una
+//función como la anterior (y sin necesidad de saber que esta contando)
+//¿Cual es el diseñode aplicar()?
+//Implementa la función aplicar().
+//Suponiendo que ya esta implementada la función aplicar(), escribe una llamada
+//para obtener las longitudes de una de las palabras de la lista.
+
+//----------------------------------Pregunta 4----------------------------------
+
+//Diseña una función que devuelva las temperaturas guardadas en una base de
+//datos que ha medido un sensor (identificar como texto).
+//Implementa la anterior función utilizando la funcion asincrona.
+//db.all(textoSQL, function(err, res){})
+//Escribe una llamada a la anterior funcion.
+
+//------------------------------------------------------------------------------
